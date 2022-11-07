b547b69cbd907a37b9637b9efb721889a78a3e3a
Marco Edi Ciani <meciani@studenti.uninsubria.it>
2022-11-07 18:43:10 +0100

    Aggiunta activity per la creazione dei menu

---
+++ app/src/main/java/it/cni/progmen/AddMenu.kt
@@ -0,0 +1,11 @@
+package it.cni.progmen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_menu)
+    }
+}

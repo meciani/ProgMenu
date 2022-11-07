30eaaac88c9c246c84c150203853df079fe92786
Marco Edi Ciani <meciani@studenti.uninsubria.it>
2022-11-07 19:18:46 +0100

    Aggiunto db Firebase

--- app/src/main/java/it/cni/progmen/ListaSpesa.kt
+++ app/src/main/java/it/cni/progmen/ListaSpesa.kt
@@ -2,10 +2,20 @@ package it.cni.progmen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 class ListaSpesa : AppCompatActivity() {
+
+    private lateinit var listaSpesaRecyclerView : RecyclerView
+    private lateinit var listAdapter : ListAdapter
+    private lateinit var spesaList : ArrayList<>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_spesa)
+
+
     }
 }
